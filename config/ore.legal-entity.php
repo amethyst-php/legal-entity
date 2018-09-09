8ae67db1a5a0ca84e854ab4833f60f56e639c39e
railken <railken@protonmail.ch>
2018-09-09 21:43:12 +0200

    better configs

--- config/ore.legal-entity.php
+++ config/ore.legal-entity.php
@@ -11,6 +11,86 @@ return [
     */
     'table' => 'ore_legal_entities',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Class Entity
+    |--------------------------------------------------------------------------
+    |
+    | The class of the model used by the manager.
+    | Change this if you have to add more relations or custom logic.
+    | The attribute $fillable is already updated by the config 'attributes'.
+    |
+    */
+    'entity' => Railken\LaraOre\LegalEntity\LegalEntity::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Manager
+    |--------------------------------------------------------------------------
+    |
+    | The class of the manager used to perform all actions by the Controller.
+    | Change this if you have to add more complex actions (.e.g. ::create).
+    | The attribute $attributes is already updated by the config 'attributes'.
+    |
+    */
+    'manager' => Railken\LaraOre\LegalEntity\LegalEntityManager::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Repository
+    |--------------------------------------------------------------------------
+    |
+    | The class of the repository used to perform all queries.
+    | Change this if you have to add more complex queries (e.g. ::findOneBy).
+    |
+    */
+    'repository' => Railken\LaraOre\LegalEntity\LegalEntityRepository::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Serializer
+    |--------------------------------------------------------------------------
+    |
+    | The class of the serializer used to serialize the model.
+    | Change this if you have to add more data while serializing.
+    | All the attributes of the manager are already included in the serializer.
+    |
+    */
+    'serializer' => Railken\LaraOre\LegalEntity\LegalEntitySerializer::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Validator
+    |--------------------------------------------------------------------------
+    |
+    | The class of the validator used to validate the parameters.
+    | Change this if you have to add more complex validation.
+    | A validation handled by the single attributes is always preferred to this.
+    |
+    */
+    'validator' => Railken\LaraOre\LegalEntity\LegalEntityValidator::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Authorizer
+    |--------------------------------------------------------------------------
+    |
+    | The class of the authorizer used to authorize the user.
+    | Change this if you have to add more complex authorization.
+    |
+    */
+    'authorizer' => Railken\LaraOre\LegalEntity\LegalEntityAuthorizer::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Extra Attributes
+    |--------------------------------------------------------------------------
+    |
+    | Here you may add all the extra attributes needed.
+    | Theese attributes will be added in the Model, Manager and Controller.
+    | A new migration is still required to update the database.
+    |
+    */
     'attributes' => [
         'code_tin'    => \Railken\LaraOre\LegalEntity\Attributes\Codes\International\TIN\TINAttribute::class,
         'code_vat'    => \Railken\LaraOre\LegalEntity\Attributes\Codes\International\VAT\VATAttribute::class,
@@ -31,7 +111,7 @@ return [
             'enabled'    => true,
             'controller' => Railken\LaraOre\Http\Controllers\Admin\LegalEntitiesController::class,
             'router'     => [
-                'prefix'      => '/admin/legal-entities',
+                'prefix'      => '/legal-entities',
             ],
         ],
     ],
