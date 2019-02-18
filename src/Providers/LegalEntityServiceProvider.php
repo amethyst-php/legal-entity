93868a152a06d36695b58a6df43dcbd50f5c70fd
railken <railken@protonmail.ch>
2019-02-18 11:40:10 +0100

    add taxonomy seeds

--- src/Providers/LegalEntityServiceProvider.php
+++ src/Providers/LegalEntityServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Railken\Amethyst\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Railken\Amethyst\Common\CommonServiceProvider;
 
 class LegalEntityServiceProvider extends CommonServiceProvider
@@ -15,5 +16,8 @@ class LegalEntityServiceProvider extends CommonServiceProvider
 
         $this->app->register(\Railken\Amethyst\Providers\AddressServiceProvider::class);
         $this->app->register(\Railken\Amethyst\Providers\TaxonomyServiceProvider::class);
+
+        Config::push('amethyst.taxonomy.data.taxonomy.seeds', ['name' => Config::get('amethyst.legal-entity.data.legal-entity.taxonomy')]);
+        Config::push('amethyst.taxonomy.data.taxonomy.seeds', ['name' => Config::get('amethyst.legal-entity.data.legal-entity-contact.taxonomy')]);
     }
 }
