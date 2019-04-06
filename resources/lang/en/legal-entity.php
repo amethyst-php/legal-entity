ff2f5981a37ed78b96308d8e6ba897a5faa5cfbc
railken <railken@protonmail.ch>
2019-04-06 22:42:00 +0200

    add lang

---
+++ resources/lang/en/legal-entity.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => 'Legal Entity',
+    'description' => 'An association, corporation, partnership, proprietorship, trust, or individual that has legal standing in the eyes of law. A legal entity has legal capacity to enter into agreements or contracts, assume obligations, incur and pay debts, sue and be sued in its own right, and to be held responsible for its actions.',
+    'attributes' => [
+        'type_id' => [
+            'label' => 'Type'
+        ]
+    ]
+]
---
+++ resources/lang/it/legal-entity.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => 'Entità Legale',
+    'description' => 'Un'associazione, una società, una società di persone o un individuo che ha una posizione legale agli occhi della legge. Un'entità legale ha la capacità giuridica di stipulare accordi o contratti, assumere obblighi, incorrere e pagare debiti, citare in giudizio ed essere citato in giudizio a titolo personale, e di essere ritenuto responsabile delle proprie azioni.',
+    'attributes' => [
+        'type_id' => [
+            'label' => 'Tipologia'
+        ]
+    ]
+]
