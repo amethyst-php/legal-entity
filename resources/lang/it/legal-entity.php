2406b3e3b1744fe97df906ccb104d9ff4d773183
railken <railken@protonmail.ch>
2019-04-14 20:44:35 +0200

    fix lang

--- resources/lang/it/legal-entity.php
+++ resources/lang/it/legal-entity.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Entità Legale',
-    'description' => 'Un'associazione, una società, una società di persone o un individuo che ha una posizione legale agli occhi della legge. Un'entità legale ha la capacità giuridica di stipulare accordi o contratti, assumere obblighi, incorrere e pagare debiti, citare in giudizio ed essere citato in giudizio a titolo personale, e di essere ritenuto responsabile delle proprie azioni.',
+    'description' => 'Un\'associazione, una società, una società di persone o un individuo che ha una posizione legale agli occhi della legge. Un\'entità legale ha la capacità giuridica di stipulare accordi o contratti, assumere obblighi, incorrere e pagare debiti, citare in giudizio ed essere citato in giudizio a titolo personale, e di essere ritenuto responsabile delle proprie azioni.',
     'attributes' => [
         'type_id' => [
             'label' => 'Tipologia'
